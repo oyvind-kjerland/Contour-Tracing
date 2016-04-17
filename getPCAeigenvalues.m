c8e2c37014a511b65a1996e48358508ba15fbf8e
Øyvind <oyvindk_93@hotmail.com>
2016-04-17 22:22:10 +0200

    Add code for finding eigenvalues from PCA

---
+++ getPCAeigenvalues.m
@@ -0,0 +1,34 @@
+function eigenvalues = getPCAeigenvalues( B )
+%GETPCAEIGENVALUES Get eigenvalues of boundary B
+
+
+% Transpose boundary
+B = transpose(B);
+B(:,[1,2]) = B(:,[2,1]); 
+
+% mean of input vectors
+ux = mean(B); 
+
+% Center around mean
+x = [B(:,1)-ux(1), B(:,2)-ux(2)];
+figure, plot(x(:,1), x(:,2), '.');
+title('Centered input vectors');
+axis equal
+
+% Find covariance matrix
+Cx = cov(x);
+
+% Find eigenvalues of covariance matrix
+[V,D] = eig(Cx); 
+hold on
+e1 = V(:,1);
+e2 = V(:,2);
+d1 = D(1,1);
+d2 = D(2,2);
+
+line([0 d1*e1(1)], [0 d1*e1(2)], 'Color', 'g', 'LineWidth', 2);
+line([0 d2*e2(1)], [0 d2*e2(2)], 'Color', 'g', 'LineWidth', 2);
+eigenvalues = D;
+
+end
+
